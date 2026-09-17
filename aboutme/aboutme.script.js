b2f25caaa8b4f100afdaf3a9e2ece543e6bffb09
Alexandra Vaughn <alexvaughn121@gmail.com>
2026-09-17 12:51:51 -0500

    Update aboutme.script.js

--- aboutme/aboutme.script.js
+++ aboutme/aboutme.script.js
@@ -1,1 +1,19 @@
+/*var app = angular.module('aMod', ['ng-route']);
 
+app.config(function($routeProvider){
+  $routeProvider.when('/', {
+    templateUrl: '/',
+    controller: 'aController'
+  });
+
+  $routeProvider.when('/index.html',{
+    templateUrl: 'mainpage/index.html',
+    controller: 'aController'
+  });
+
+  $routeProvider.when('/aboutme.html',{
+    templateUrl: 'aboutme/aboutme.html',
+    controller: 'aController'
+  });
+
+});*/
